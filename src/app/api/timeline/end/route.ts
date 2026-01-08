8015bed3cbb2a17e4c1df178f5445c23950336cd
mizunatoma <k40117135@gmail.com>
2026-01-08 19:18:32 +0900

    feat: add timeline end API

--- src/app/api/timeline/end/route.ts
+++ src/app/api/timeline/end/route.ts
@@ -2,15 +2,51 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from '@/utils/prisma' ;
 import { verifyAuth } from "@/utils/verifyAuth";
-import { request } from "http";
 
 // ===============================
 // POST
 // ===============================
-export const POST = async (request: NextRequest) => {
+export const POST = async (request: NextRequest ) => {
+  // 認証・userId取得
+  const authResult = await verifyAuth(request);
+  if (authResult instanceof NextResponse) return authResult;
+  const userId = authResult.user.id;
 
+  // profile 取得
+  const profile = await prisma.profile.findUnique({
+    where: { userId },
+  })
 
+  if (!profile) {
+    return NextResponse.json(
+      { error: "Profile not found" }, { status: 400 },
+    );
+  }
 
+  // 進行中のアクティビティを探す
+  const runningLog = await prisma.timeLog.findFirst({
+    where: {
+      endAt: null,
+      activity: {
+        profileId: profile.id
+      }
+    }
+  })
 
-  
-}
+    if (!runningLog) {
+    return NextResponse.json(
+      { error: "runningLog not found" }, { status: 400 },
+    );
+  }
+
+  // 見つかった timelog の id を使って終了時間を更新
+  const timeLog = await prisma.timeLog.update({
+    where: { id: runningLog.id },
+    data: { endAt: new Date()}
+  });
+
+  return NextResponse.json(
+    { timeLog }, { status: 200}
+  );
+};
+
