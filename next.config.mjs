2fc88ca51e265711b8e0d753f88a9c4113441234
mizuna toma <k4017135@gmail.com>
2026-01-31 17:59:20 +0900

    feat: WSLでのホットリロード用にポーリング設定を追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000, // １秒ごとにファイルの変更をチェックする
+      aggregateTimeout: 300,
+    }
+    return config
+  },
+};
 
 export default nextConfig;
