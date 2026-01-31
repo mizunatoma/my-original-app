03a7624df6d3e0fe02058dc98012337fbfd5ef94
mizuna toma <k4017135@gmail.com>
2026-01-31 19:18:34 +0900

    chore: WSL/Linux環境向けに最適化

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.watchOptions = {
-      poll: 1000, // １秒ごとにファイルの変更をチェックする
-      aggregateTimeout: 300,
-    }
-    return config
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
