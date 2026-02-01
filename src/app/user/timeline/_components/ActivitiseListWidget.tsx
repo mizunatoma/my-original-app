b4c3db63755cb09fe368a9b387aa7f87b194d16c
mizuna toma <k4017135@gmail.com>
2026-02-01 14:24:15 +0900

    chore: ウィジェットUIのリネーム

--- src/app/user/timeline/_components/ActivitiseListWidget.tsx
+++ src/app/user/timeline/_components/ActivitiseListWidget.tsx
@@ -2,7 +2,7 @@
 
 import React from "react"
 
-export default function ActivityShortcutsWidget() {
+export default function ActivitiseListWidget() {
   return (
     <div className="widget-card">
 
--- src/app/user/timeline/page.tsx
+++ src/app/user/timeline/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import CurrentActivityWidget from "./_components/CurrentActivityWidget"
-import ActivityShortcutsWidget from "./_components/ActivityShortcutsWidget"
+import ActivitiseListWidget from "./_components/ActivitiseListWidget"
 
 export default function Page() {
   return (
     <div className="flex flex-control items-center pt-10 px-4">
       <div className="w-full max-w-3xl">
         <CurrentActivityWidget />
-        <ActivityShortcutsWidget />
+        <ActivitiseListWidget />
       </div>
     </div>
   )
