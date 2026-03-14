007ebc4ab0ec6bdcfaf8cb7e821e50ad0ed3508f
mizuna toma <k4017135@gmail.com>
2026-03-14 21:13:34 +0900

    feat: PUT /api/todo-lists/[listId] を実装

--- src/app/api/todo-lists/route.ts
+++ src/app/api/todo-lists/route.ts
@@ -2,7 +2,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from '@/app/_utils/prisma'
 import { getAuthUser } from "@/app/_utils/getAuthUser";
-import { request } from "http";
 
 // ===============================
 // GET
