7969f2c5efba07eacb07133e9e13b4b790f2216a
mizuna toma <k4017135@gmail.com>
2026-03-14 15:35:16 +0900

    feat: GET /api/todo-lists を実装

--- src/app/api/todo-lists/route.ts
+++ src/app/api/todo-lists/route.ts
@@ -6,9 +6,25 @@ import { getAuthUser } from "@/app/_utils/getAuthUser";
 // ===============================
 // GET
 // ===============================
+export const GET = async (request: NextRequest) => {
+  try {
+    const auth = await getAuthUser();
+    if (auth instanceof NextResponse) return auth; // instanceof は「この値は○○の種類か？」を確認する構文
+    const user = auth.user;
 
+    const todoList = await prisma.todoList.findMany({
+      where: {
+        profile: { userId: user.id },
+        deletedAt: null,
+      },
+    })
 
-
+    return NextResponse.json({ todoList }, { status: 200 })
+  } catch (e) {
+    console.error("GET /api/todo-list:", e);
+    return NextResponse.json({ error: String(e) }, { status: 500 }) // 500＝サーバー側で予期しないエラーが起きた
+  };
+};
 
 // ===============================
 // POST
