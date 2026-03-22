895de69b2f51668506d7725b0eb76344719a70df
mizuna toma <k4017135@gmail.com>
2026-03-22 09:43:40 +0900

    feat: TodoパネルUI実装（リスト追加フォーム）

--- src/app/user/_components/TodoPanel.tsx
+++ src/app/user/_components/TodoPanel.tsx
@@ -12,19 +12,33 @@ const fetcher = (url: string) => fetch(url).then(res => res.json())
 
 export default function TodoPanel({ isCollapsed, isTodoPanelOpen }: TodoPanelProps) {
   const [selectedListId, setSelectedListId] = useState<string | null>(null);
-  const [todo, setTodo] = useState("")
-  const { data: list } = useSWR<TodoListsAPI.Get.Response>('/api/todo-lists', fetcher)
+  const [editingTodo, setEditingTodo] = useState("")
+  const [editingList, setEditingList] = useState("")
+  const [isOpen, setIsOpen] = useState(false);
+  const { data: list, mutate: mutateList } = useSWR<TodoListsAPI.Get.Response>('/api/todo-lists', fetcher)
   const { data: todos, mutate: mutateTodo } = useSWR<TodoItemsAPI.Get.Response>(`/api/todo-lists/${selectedListId}/todos`, fetcher)
 
-  const handleAddSave = async (title: string) => {
-    console.log(selectedListId)
+  // todoの追加
+  const handleAddTodo = async (title: string) => {
     await fetch(`/api/todo-lists/${selectedListId}/todos`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ title })
     })
     mutateTodo()
-    setTodo("")
+    setEditingTodo("")
+  }
+
+  // listの追加
+  const handleAddList = async (name: string) => {
+    await fetch(`/api/todo-lists`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name })
+    })
+    mutateList()
+    mutateTodo()
+    setEditingList("")
   }
 
   useEffect(() => {
@@ -51,21 +65,42 @@ export default function TodoPanel({ isCollapsed, isTodoPanelOpen }: TodoPanelPro
 
       <button
         className='inline-flex items-center px-3 py-1 rounded-t-lg text-sm font-medium bg-orange-200 text-orange-800'
-        onClick={() => { }}
+        onClick={() => setIsOpen(true)}
       >
         +
       </button>
 
+      {isOpen &&
+        <div>
+          <input
+            className='border border-gray-400 rounded p-2'
+            placeholder='新しいTodoList'
+            onChange={(e) => setEditingList(e.target.value)}
+            value={editingList}
+          />
+          <button
+            className='rounded-lg p-1 text-center text-sm text-[#5A8B7D] border border-[#5A8B7D] hover:bg-[#F2F0E9]'
+            disabled={!editingList.trim()}
+            onClick={() => {
+              handleAddList(editingList)
+              setIsOpen(false)
+            }}
+
+          >
+            追加
+          </button>
+        </div>}
+
       <div className='flex'>
         <input
-          className='boder border-gray-100 rounded '
+          className='boder border-gray-100 rounded'
           placeholder='新しいTodo'
-          onChange={(e) => setTodo(e.target.value)}
-          value={todo}
+          onChange={(e) => setEditingTodo(e.target.value)}
+          value={editingTodo}
         />
         <button
           className='rounded-lg p-1 text-center text-sm text-[#5A8B7D] border border-[#5A8B7D] hover:bg-[#F2F0E9]'
-          onClick={() => handleAddSave(todo)}
+          onClick={() => handleAddTodo(editingTodo)}
         >
           追加
         </button>
