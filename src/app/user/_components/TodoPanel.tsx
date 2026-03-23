11e4ba0e678375a574b7d147ad11cad68c357356
mizuna toma <k4017135@gmail.com>
2026-03-23 19:18:53 +0900

    feat: TodoPanel UIブラッシュアップ

--- src/app/user/_components/TodoPanel.tsx
+++ src/app/user/_components/TodoPanel.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react'
 import { TodoListsAPI, TodoItemsAPI } from '@/types/api'
 import useSWR from 'swr'
-import { Trash2, SquarePen } from 'lucide-react'
+import { Trash2, SquarePen, Check, X } from 'lucide-react'
 
 interface TodoPanelProps {
   isCollapsed: boolean
@@ -92,75 +92,80 @@ export default function TodoPanel({
 
   return (
     <aside
-      className={`fixed bottom-0 top-0 z-20 overflow-auto bg-[#FCFAF7] transition-all duration-300 ${isCollapsed ? 'left-[80px]' : 'left-[160px]'} ${isTodoPanelOpen ? 'w-[300px] border border-[#EFEDE6] p-4' : 'w-[0px]'}`}
+      className={`fixed bottom-0 top-0 z-20 space-y-2 overflow-auto bg-[#FCFAF7] transition-all duration-300 ${isCollapsed ? 'left-[80px]' : 'left-[160px]'} ${isTodoPanelOpen ? 'w-[300px] border border-[#e9e3cc] p-4' : 'w-[0px]'}`}
     >
       {/*listチップ一覧*/}
-      {list?.todoLists?.map((list) => (
-        <button
-          key={list.id}
-          className={`inline-flex items-center rounded-xl bg-orange-100 px-2 text-sm font-medium text-orange-800 ${list.id === selectedListId && 'border-2 border-orange-200'}`}
-          onClick={() => setSelectedListId(list.id)}
-        >
-          {list.name}
-        </button>
-      ))}
+      <div className="flex flex-wrap gap-1">
+        {list?.todoLists?.map((list) => (
+          <button
+            key={list.id}
+            className={`inline-flex items-center rounded-xl border px-2 text-sm font-medium ${list.id === selectedListId ? 'bg-[#5A8B7D]/70 text-white' : 'border border-[#5A8B7D]/70 text-[#5A8B7D]/70 hover:bg-[#5A8B7D]/70 hover:text-white'}`}
+            onClick={() => setSelectedListId(list.id)}
+          >
+            {list.name}
+          </button>
+        ))}
 
-      {/*list追加*/}
-      <button
-        className="inline-flex items-center rounded-t-lg bg-orange-200 px-3 py-1 text-sm font-medium text-orange-800"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        +
-      </button>
+        {/*list追加*/}
+        <button
+          className="items-center rounded-xl border-[#5A8B7D]/70 px-2 text-[#5A8B7D] hover:bg-[#5A8B7D]/70 hover:text-white"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          +
+        </button>
+      </div>
 
       {isOpen && (
-        <div>
+        <div className="flex gap-2">
           <input
-            className="rounded border border-gray-400 p-2"
-            placeholder="新しいTodoList"
+            className="flex-1 rounded border-2 border-[#5A8B7D] p-1 focus:outline-none focus:ring-1 focus:ring-[#5A8B7D]"
+            placeholder="新しいリスト"
             onChange={(e) => setNewList(e.target.value)}
             value={newList}
           />
           <button
-            className="rounded-lg border border-[#5A8B7D] p-1 text-center text-sm text-[#5A8B7D] hover:bg-[#F2F0E9]"
+            className="rounded-lg border-2 border-[#5A8B7D] p-2 text-center text-xs text-[#5A8B7D] hover:bg-[#5A8B7D]/70 hover:text-white"
             disabled={!newList.trim()}
             onClick={() => {
               handleAddList(newList)
               setIsOpen(false)
             }}
           >
-            追加
+            作成
           </button>
         </div>
       )}
 
       {/*todo追加*/}
       {selectedListId !== null && (
-        <div className="flex">
+        <div className="flex gap-2">
           <input
-            className="rounded border border-gray-100"
+            className="flex-1 rounded border border-[#5A8B7D] p-1 focus:outline-none focus:ring-1 focus:ring-[#5A8B7D]"
             placeholder="新しいTodo"
             onChange={(e) => setNewTodo(e.target.value)}
             value={newTodo}
           />
           <button
-            className="rounded-lg border border-[#5A8B7D] p-1 text-center text-sm text-[#5A8B7D] hover:bg-[#F2F0E9]"
+            className="rounded-lg border border-[#5A8B7D] p-2 text-center text-sm text-[#5A8B7D] hover:bg-[#5A8B7D]/70 hover:text-white"
             onClick={() => handleAddTodo(newTodo)}
             disabled={!newTodo.trim()}
           >
-            追加
+            +
           </button>
         </div>
       )}
 
       {/*todo一覧*/}
-      <ul>
+      <ul className="space-y-2">
         {(todos?.todos || []).map((todo) => (
-          <li key={todo.id} className="flex items-center gap-2 py-1">
+          <li
+            key={todo.id}
+            className="flex items-center gap-2 rounded-xl bg-white p-2 shadow-sm"
+          >
             {editingId !== todo.id ? (
               /* 通常モードの表示 */
               <>
-                <label>
+                <label className="flex flex-1 items-center gap-2">
                   <input
                     type="checkbox"
                     checked={todo.isDone}
@@ -174,23 +179,29 @@ export default function TodoPanel({
                     {todo.title}
                   </span>
                 </label>
-                <button
-                  onClick={() => {
-                    setEditingId(todo.id) // 編集モードにセット
-                    setEditingTitle(todo.title) // 今のタイトル初期値をセット
-                  }}
-                >
-                  <SquarePen
-                    size={16}
-                    className="text-gray-500 hover:text-orange-200"
-                  />
-                </button>
-                <button
-                  className="text-red-400"
-                  onClick={() => handleDeleteTodo(todo.id)}
-                >
-                  <Trash2 size={16} />
-                </button>
+
+                <div className="flex justify-end gap-3 p-2 text-right">
+                  <button
+                    onClick={() => {
+                      setEditingId(todo.id) // 編集モードにセット
+                      setEditingTitle(todo.title) // 今のタイトル初期値をセット
+                    }}
+                  >
+                    <SquarePen
+                      size={16}
+                      className="text-gray-500 hover:text-[#5A8B7D]/70"
+                    />
+                  </button>
+                  <button
+                    className="text-red-400"
+                    onClick={() => handleDeleteTodo(todo.id)}
+                  >
+                    <Trash2
+                      size={16}
+                      className="text-gray-500 hover:text-red-400"
+                    />
+                  </button>
+                </div>
               </>
             ) : (
               /* 編集モードの表示 */
@@ -199,7 +210,7 @@ export default function TodoPanel({
                   type="text"
                   value={editingTitle}
                   onChange={(e) => setEditingTitle(e.target.value)}
-                  className="flex-1 rounded border px-2 py-1"
+                  className="flex-1 rounded border px-2 py-1 focus:outline-none focus:ring-1 focus:ring-[#5A8B7D]"
                   autoFocus
                 />
                 <button
@@ -208,9 +219,11 @@ export default function TodoPanel({
                     setEditingId(null) // 編集モード終了
                   }}
                 >
-                  保存
+                  <Check size={16} className="text-[#5A8B7D]/70" />
                 </button>
-                <button onClick={() => setEditingId(null)}>キャンセル</button>
+                <button onClick={() => setEditingId(null)}>
+                  <X size={16} className="text-red-400" />
+                </button>
               </div>
             )}
           </li>
@@ -220,11 +233,13 @@ export default function TodoPanel({
       {/*list削除*/}
       {selectedListId !== null && (
         <button
-          className="mt-auto flex w-full items-center justify-end gap-1 p-2 text-right text-red-400 hover:text-red-600"
+          className="mt-auto flex w-full justify-end gap-1 p-2 text-right"
           onClick={handleDeleteList}
         >
-          <Trash2 size={16} />
-          <span>リストを削除</span>
+          <div className="flex items-center text-gray-500 hover:text-red-400">
+            <Trash2 size={16} />
+            <span>リストを削除</span>
+          </div>
         </button>
       )}
     </aside>
--- src/app/user/timeline/_components/CurrentCategoryWidget.tsx
+++ src/app/user/timeline/_components/CurrentCategoryWidget.tsx
@@ -1,17 +1,20 @@
 'use client'
-import React, { useState, useEffect } from "react"
-import { Dispatch, SetStateAction } from "react"
-import { TimelineAPI } from "@/types/api"
+import React, { useState, useEffect } from 'react'
+import { Dispatch, SetStateAction } from 'react'
+import { TimelineAPI } from '@/types/api'
 
 type Props = {
-  currentCategoryID: { id: string, count: number }
+  currentCategoryID: { id: string; count: number }
   onPressStopButton: Dispatch<SetStateAction<{ count: number }>>
 }
 
-export default function CurrentCategoryWidget({ currentCategoryID, onPressStopButton }: Props) {
-  const [data, setData] = useState<TimelineAPI.Running.Response | null>(null);
-  const [elapsed, setElapsed] = useState(0);
-  const [isLoading, setIsloading] = useState(true);
+export default function CurrentCategoryWidget({
+  currentCategoryID,
+  onPressStopButton,
+}: Props) {
+  const [data, setData] = useState<TimelineAPI.Running.Response | null>(null)
+  const [elapsed, setElapsed] = useState(0)
+  const [isLoading, setIsloading] = useState(true)
 
   const fetchRunning = async () => {
     setIsloading(true)
@@ -33,13 +36,12 @@ export default function CurrentCategoryWidget({ currentCategoryID, onPressStopBu
       await fetch('/api/timeline/start', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ activityId: currentCategoryID.id })
+        body: JSON.stringify({ activityId: currentCategoryID.id }),
       })
       fetchRunning()
     }
     start()
   }, [currentCategoryID])
-
 
   // 経過時間の表示
   useEffect(() => {
@@ -49,7 +51,7 @@ export default function CurrentCategoryWidget({ currentCategoryID, onPressStopBu
       const now = new Date()
       const start = new Date(data.log.startAt)
       const minutes = Math.floor((now - start) / 60000)
-      setElapsed(minutes);
+      setElapsed(minutes)
     }, 60000) // 1分ごと
 
     return () => clearInterval(timer)
@@ -59,47 +61,57 @@ export default function CurrentCategoryWidget({ currentCategoryID, onPressStopBu
     <div>
       <div className="widget-card">
         <h2 className="section-title">現在のCategory</h2>
-        {!isLoading
-          ? (
-            <div>
-              < div className="flex flex-col gap-3 bg-rose-50 rounded-xl p-4 mb-4">
-                <div className="flex items-center gap-3 w-full">
+        {!isLoading ? (
+          <div>
+            <div className="mb-4 flex flex-col gap-3 rounded-xl bg-[#5D866C]/10 p-4">
+              <div className="flex w-full items-center gap-3">
+                {data?.running ? (
+                  <div>
+                    <div className="flex items-center gap-2">
+                      <div
+                        className={`h-3 w-3 flex-shrink-0 rounded-full shadow-sm ${data.log.colorToken}`}
+                      />
+                      <span>{data.log.activityName}</span>
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <span className="text-xs text-gray-500">
+                        開始:{' '}
+                        {new Date(data.log.startAt).toLocaleTimeString(
+                          'ja-JP',
+                          { hour: '2-digit', minute: '2-digit' },
+                        )}{' '}
+                      </span>
+                      <span className="text-xs text-gray-500">
+                        経過: {elapsed}分
+                      </span>
+                    </div>
+                  </div>
+                ) : (
+                  <span>実行中なし</span>
+                )}
+              </div>
 
-                  {data?.running
-                    ? (
-                      <div>
-                        <div className="flex items-center gap-2">
-                          <div className={`w-3 h-3 rounded-full shadow-sm flex-shrink-0 ${data.log.colorToken}`} />
-                          <span>{data.log.activityName}</span>
-                        </div>
-                        <div className="flex items-center gap-2">
-                          <span className="text-xs text-gray-500">開始: {new Date(data.log.startAt).toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit' })} </span>
-                          <span className="text-xs text-gray-500">経過: {elapsed}分</span>
-                        </div>
-                      </div>
-                    )
-                    : (<span>実行中なし</span>)
-                  }
-                </div>
+              {data?.running && (
+                <div className="">
+                  <button
+                    className="button mt-1 w-full bg-rose-200 text-rose-800 hover:bg-rose-300"
+                    disabled={!data?.running}
+                    onClick={async () => {
+                      onPressStopButton((s) => ({ count: s.count + 1 }))
+                      await fetch('/api/timeline/end', {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify({
+                          activityId: data?.running && data.log.activityId,
+                        }),
+                      })
+                      setData({ running: false })
+                    }}
+                  >
+                    停止
+                  </button>
 
-                {data?.running &&
-                  <div className="">
-                    <button
-                      className="button bg-rose-200 hover:bg-rose-300 text-rose-800 w-full mt-1"
-                      disabled={!data?.running}
-                      onClick={async () => {
-                        onPressStopButton(s => ({ count: s.count + 1 }))
-                        await fetch('/api/timeline/end', {
-                          method: 'POST',
-                          headers: { 'Content-Type': 'application/json' },
-                          body: JSON.stringify({ activityId: data?.running && data.log.activityId }),
-                        })
-                        setData({ running: false }
-                        )
-                      }}
-                    >停止</button>
-
-                    {/* 今後実装予定
+                  {/* 今後実装予定
                     < div className="relative mt-4 ">
                       <textarea
                         className="w-full h-24 bg-gray-50 rounded-xl p-4 text-gray-700 border-rose-100 border-2 focus:outline-none focus:ring-2 focus:ring-rose-100 placeholder-gray-400"
@@ -107,14 +119,13 @@ export default function CurrentCategoryWidget({ currentCategoryID, onPressStopBu
                       />
                     </div>
                     */}
-
-                  </div>
-                }
-              </div>
-            </div >
-          )
-          : (<p>読み込み中…</p>)
-        }
+                </div>
+              )}
+            </div>
+          </div>
+        ) : (
+          <p>読み込み中…</p>
+        )}
       </div>
     </div>
   )
