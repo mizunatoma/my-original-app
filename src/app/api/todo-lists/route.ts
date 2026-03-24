9f0bb625a79ddb5e808ce52693fdda08b7b62e8a
mizuna toma <k4017135@gmail.com>
2026-03-24 11:30:38 +0900

    fix: GET /api/todo-lists のレスポンスに型を付与

--- src/app/api/todo-lists/route.ts
+++ src/app/api/todo-lists/route.ts
@@ -1,30 +1,43 @@
 // /api/todo-lists
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/app/_utils/prisma'
-import { getAuthUser } from "@/app/_utils/getAuthUser";
+import { getAuthUser } from '@/app/_utils/getAuthUser'
+import { TodoListsAPI } from '@/types/api'
 
 // ===============================
 // GET
 // ===============================
 export const GET = async (request: NextRequest) => {
   try {
-    const auth = await getAuthUser();
-    if (auth instanceof NextResponse) return auth;
-    const user = auth.user;
+    const auth = await getAuthUser()
+    if (auth instanceof NextResponse) return auth
+    const user = auth.user
 
-    const todoLists = await prisma.todoList.findMany({
+    const todosLists = await prisma.todoList.findMany({
       where: {
         profile: { userId: user.id },
         deletedAt: null,
       },
     })
 
-    return NextResponse.json({ todoLists }, { status: 200 })
+    const todoLists = todosLists.map((todoList) => ({
+      id: todoList.id,
+      profileId: todoList.profileId,
+      name: todoList.name,
+      sortOrder: todoList.sortOrder,
+      createdAt: todoList.createdAt.toISOString(),
+      updatedAt: todoList.updatedAt.toISOString(),
+    }))
+
+    return NextResponse.json<TodoListsAPI.Get.Response>(
+      { todoLists },
+      { status: 200 },
+    )
   } catch (e) {
-    console.error("GET /api/todo-list:", e);
+    console.error('GET /api/todo-lists:', e)
     return NextResponse.json({ error: String(e) }, { status: 500 }) // 500＝サーバー側で予期しないエラーが起きた
-  };
-};
+  }
+}
 
 // ===============================
 // POST
@@ -32,19 +45,22 @@ export const GET = async (request: NextRequest) => {
 export const POST = async (request: NextRequest) => {
   try {
     const auth = await getAuthUser()
-    if (auth instanceof NextResponse) return auth;
-    const user = auth.user;
+    if (auth instanceof NextResponse) return auth
+    const user = auth.user
 
     const profile = await prisma.profile.findFirst({
       where: {
-        userId: user.id
-      }
+        userId: user.id,
+      },
     })
 
     if (!profile) {
-      return NextResponse.json({ error: "Authorization failure" }, { status: 403 });
+      return NextResponse.json(
+        { error: 'Authorization failure' },
+        { status: 403 },
+      )
     }
-    const { name } = await request.json();
+    const { name } = await request.json()
 
     const todoList = await prisma.todoList.create({
       data: {
@@ -52,25 +68,11 @@ export const POST = async (request: NextRequest) => {
         profileId: profile.id,
         sortOrder: 0,
       },
-    });
+    })
 
-    return NextResponse.json({ todoList }, { status: 201 }); // 201=成功(新規作成)
+    return NextResponse.json({ todoList }, { status: 201 }) // 201=成功(新規作成)
   } catch (e) {
-    console.error("POST /api/todo-list error:", e);
+    console.error('POST /api/todo-list error:', e)
     return NextResponse.json({ error: String(e) }, { status: 500 })
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
