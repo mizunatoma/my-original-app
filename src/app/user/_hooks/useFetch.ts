1ce4c1d131aa1b58d9aa3b94eeea7e904ab0a87d
mizuna toma <k4017135@gmail.com>
2026-03-31 11:23:13 +0900

    chore: useFetchカスタムフックを追加

---
+++ src/app/user/_hooks/useFetch.ts
@@ -0,0 +1,16 @@
+// SWR共通化hook
+import useSWR from 'swr'
+
+// nullを渡したときは、fetchをスキップ
+export function useFetch<T>(endpoint: string | null) {
+  // urlを受け取り、型はジェネリクスで約束する。エラーハンドリングも明示
+  const fetcher = async (url: string): Promise<T> => {
+    const res = await fetch(url)
+    if (!res.ok) throw new Error('データ取得に失敗しました')
+    return (await res.json()) as T
+  }
+
+  // useSWRにendpoint、fetcherを渡す
+  const { data, error, isLoading, mutate } = useSWR<T>(endpoint, fetcher)
+  return { data, error, isLoading, mutate }
+}
